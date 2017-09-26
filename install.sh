d87ac52718312e580d891c42c255e3792e80a8e3
piih~ <piih_@live.com>
2017-09-26 03:40:43 -0300

    Improves to installer
    Add some more expressive functions
    
    Add --no-backup option
    
    Install plugins automatically after vimfiles install

--- install.sh
+++ install.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 
+function existFile {
+  file=$1
+  if [ -f $file ]
+  then
+    return 0
+  fi
+
+  return 1
+}
+
+function existSymlink {
+  symlink=$1
+  if [ -h $symlink ]
+  then
+    return 0
+  fi
+
+  return 1
+}
+
+function existDirectory {
+  directory=$1
+  if [ -d $directory ]
+  then
+    return 0
+  fi
+
+  return 1
+}
+
 script_dir=$(dirname $0)
+
+no_backup=$2
+
+if [ -z $no_backup ]
+then
+  let no_backup="null"
+fi
 
 if [ "vim" == $1 ]
 then
@@ -10,10 +47,10 @@ then
     vim_dir=~/.vim
     vim_dir_bkp=~/.vim.bkp
 
-    if [ -d $vim_dir ]
+    if existDirectory $vim_dir && [ "--no-backup" != $no_backup ]
     then
         COUNTER=0
-        while [ -d $vim_dir_bkp ]
+        while existDirectory $vim_dir_bkp 
         do
             let COUNTER=COUNTER+1
             vim_dir_bkp="$vim_dir.bkp$COUNTER"
@@ -21,14 +58,24 @@ then
         mv $vim_dir $vim_dir_bkp
     fi
 
+    if [ "--no-backup" == $no_backup ]
+    then
+      rm -rf ~/.vim
+    fi
+
     mkdir ~/.vim
 
     cp -r $script_dir/* ~/.vim
 
-    if [ -f $vim_file ]
+    if existSymlink ~/.vimrc
+    then
+      rm ~/.vimrc 
+    fi
+
+    if existFile $vim_file && [ "--no-backup" != $no_backup ]
     then
         COUNTER=0
-        while [ -f $vim_file_bkp ]
+        while existFile $vim_file_bkp
         do
             let COUNTER=COUNTER+1
             vim_file_bkp="$vim_file.bkp$COUNTER"
@@ -39,4 +86,6 @@ then
     ln -s ~/.vim/vimrc ~/.vimrc
 
     rm ~/.vim/install.sh
+
+    vim -c "PlugInstall|:qa"
 fi
